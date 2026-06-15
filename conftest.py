18775a0792f5b4e563bb3e3c409ca114b46edb77
suryae <eksurya27@gmail.com>
2026-06-15 19:13:59 +0530

    add some tests, increase timeouts

--- conftest.py
+++ conftest.py
@@ -1,10 +1,12 @@
+import os
+
 import pytest
 from playwright.sync_api import sync_playwright, Browser, BrowserContext
 
 @pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as p:
-        b = p.chromium.launch(headless=True, slow_mo=1000)
+        b = p.chromium.launch(headless=False, slow_mo=1000)
         yield b
         b.close()
 
@@ -17,7 +19,13 @@ def context(browser: Browser):
 @pytest.fixture(scope="session")
 def page(context: BrowserContext):
     page = context.new_page()
+    username = os.getenv("username")
+    password = os.getenv("password")
     page.goto("https://www.saucedemo.com/")
+    page.locator("//input[@data-test='username']").fill(username)
+    page.locator("//input[@data-test='password']").fill(password)
+    page.locator("//input[@data-test='login-button']").click()
+    page.wait_for_url("https://www.saucedemo.com/inventory.html")
     yield page
     page.close()
 
--- src/pages/inventory_page.py
+++ src/pages/inventory_page.py
@@ -1,26 +1,51 @@
 from playwright.sync_api import Page
 
-from src.locators.inventory_page_locators import DashboardLocators, SidebarMenuLocators
+from src.locators.inventory_page_locators import DashboardLocators, SidebarMenuLocators, InventoryItemLocators
 
 
 class InventoryPage:
     def __init__(self, page: Page):
         self.page = page
         self.dashboard_locators = DashboardLocators()
+        self.inventory_item_locators = InventoryItemLocators()
 
     def wait_for_dashboard_to_be_visible(self) -> None:
         self.page.locator(self.dashboard_locators.dashboard_header_xpath).wait_for(
-            state="visible"
+            state="visible", timeout=10000
         )
 
     def is_dashboard_visible(self) -> bool:
         return self.page.locator(
             self.dashboard_locators.dashboard_header_xpath
-        ).is_visible()
+        ).is_visible(timeout=10000)
 
     def click_on_menu_button(self) -> None:
-        self.page.locator(self.dashboard_locators.dashboard_menu_button_xpath).click()
+        self.page.locator(self.dashboard_locators.dashboard_menu_button_xpath).click(timeout=10000)
 
+    def click_on_product_sort_container(self):
+        self.page.locator(self.dashboard_locators.product_sort_container_xpath).click(timeout=10000)
+
+    def get_price_of_all_listed_items(self) -> list[float]:
+        float_prices = []
+        prices = (self.page.locator(self.inventory_item_locators.inventory_item_price_xpath)).all_text_contents()
+        for price in prices:
+            float_prices.append(price.lstrip('$'))
+        return float_prices
+
+    def get_name_of_all_listed_items(self) -> list[str]:
+        return (self.page.locator(self.inventory_item_locators.inventory_item_name_xpath)).all_text_contents()
+
+    def select_sort_by_name_ascending_order(self):
+        self.page.locator(self.dashboard_locators.product_sort_container_xpath).select_option(label="Name (A to Z)", timeout=10000)
+
+    def select_sort_by_name_descending_order(self):
+        self.page.locator(self.dashboard_locators.product_sort_container_xpath).select_option(label="Name (Z to A)", timeout=10000)
+
+    def select_sort_by_price_ascending_order(self):
+        self.page.locator(self.dashboard_locators.product_sort_container_xpath).select_option(label="Price (low to high)", timeout=10000)
+
+    def select_sort_by_price_descending_order(self):
+        self.page.locator(self.dashboard_locators.product_sort_container_xpath).select_option(label="Price (high to low)]", timeout=10000)
 
 class InventorySidebarPage:
     def __init__(self, page: Page):
@@ -28,4 +53,4 @@ class InventorySidebarPage:
         self.menu_locators = SidebarMenuLocators()
 
     def click_on_logout_link_button(self):
-        self.page.locator(self.menu_locators.logout_link_button_xpath).click()
+        self.page.locator(self.menu_locators.logout_link_button_xpath).click(timeout=10000)
